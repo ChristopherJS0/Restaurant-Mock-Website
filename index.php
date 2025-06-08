36b4a7b7fe6ea0abd4f1acacb037042936d6a654
Chris Salinas <chrisjsalinas@outlook.com>
2025-06-08 15:09:02 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Salinas Restaurant</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <?php
+      echo "<p>Today is " . date("l") . "</p>";
+    ?>
+  </body>
+</html>
